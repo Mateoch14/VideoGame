e728d162bd7f18894cfb9cd9a8651aef17e98e10
Mateoch14 <89880893+Mateoch14@users.noreply.github.com>
2023-08-11 16:10:45 -0300

    modelo creado

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,35 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey:true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull:flase
+    },
+    plataformas: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagen:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Fechadelanzamiento: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Rating: {
+      type: DataTypes.INT,
+      allowNull: false,
+    }
   });
 };
